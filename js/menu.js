e12b3e624c0bfc446b6b92ed3be691d00f9945c8
Sergio Fernandez <sergio@kasfactory.net>
2016-12-12 22:32:02 +0100

    Added scroll spy

--- js/menu.js
+++ js/menu.js
@@ -15,18 +15,18 @@ for (var i = 0; i < navbarItems.length; i++) {
 }
 
 function getElementByIdAndScroll(name) {
-	var element;
-	if (name == '') {
-		element = document.getElementsByClassName('header')[0];
-	} else {
-		element = document.getElementById(name);
-	}
+    var element;
+    if (name == '') {
+        element = document.getElementsByClassName('header')[0];
+    } else {
+        element = document.getElementById(name);
+    }
 
-	scrollToElement(element);
+    scrollToElement(element);
 }
 
 function scrollToElement(element) {
-	var jump = parseInt(element.getBoundingClientRect().top * 0.3)
+	var jump = parseInt(element.getBoundingClientRect().top * 0.3);
 	document.body.scrollTop += jump;
 
 	if(!element.lastJump || element.lastJump > Math.abs(jump)) {
@@ -41,8 +41,43 @@ function scrollToElement(element) {
 }
 
 function deleteActiveClass() {
-	/*for (var i = 0; i < navbarItems.length; i++) {
-		navbarItems[i].classList.remove('active');
-	}*/
 	document.getElementsByClassName('navbar-item active')[0].classList.remove('active');
 }
+
+var cumulativeOffset = function(element) {
+	var top = 0;
+	do {
+		top += element.offsetTop || 0;
+		element = element.offsetParent;
+	} while(element);
+
+	return top;
+};
+
+var offsetQuienSoy = cumulativeOffset(document.getElementById('quien-soy')) - 59;
+var offsetEquipo = cumulativeOffset(document.getElementById('equipo')) - 59;
+var offsetTransporte = cumulativeOffset(document.getElementById('transporte')) - 59;
+
+window.addEventListener('scroll', changeMenuStyle);
+
+function changeMenuStyle(event) {
+	if (window.pageYOffset >= 0 && window.pageYOffset < offsetQuienSoy) {
+		deleteActiveClass();
+		document.querySelector('a[href="#"]').parentNode.classList.add("active");
+	} else if(window.pageYOffset >= offsetQuienSoy && window.pageYOffset < offsetEquipo) {
+		deleteActiveClass();
+		document.querySelector('a[href$="quien-soy"]').parentNode.classList.add("active");
+	} else if (window.pageYOffset >= offsetEquipo && window.pageYOffset < offsetTransporte) {
+		deleteActiveClass();
+		document.querySelector('a[href$="equipo"]').parentNode.classList.add("active");
+	} else if (window.pageYOffset >= offsetTransporte) {
+		deleteActiveClass();
+		document.querySelector('a[href$="transporte"]').parentNode.classList.add("active");
+	}
+}
+
+
+
+
+
+
