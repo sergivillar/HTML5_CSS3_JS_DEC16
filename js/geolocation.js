c288ce1f2b2f1d1d5b21886afbe07006ef75b633
Sergio Fernandez <sergio@kasfactory.net>
2016-12-13 23:34:09 +0100

    Added AJAX request!

--- js/geolocation.js
+++ js/geolocation.js
@@ -7,24 +7,67 @@ function getLocation(){
 }
 
 function showPosition(position) {
-	console.log(position);
+	var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude + "&sensor=true";
+	var xhr;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xhr.open("GET", url, true);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			console.log(xhr);
+			console.log("Respuesta servidor", JSON.parse(xhr.responseText));
+		}
+	}
+
+	xhr.send();
 }
 
 function errorHandler(error) {
 	switch(error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("An unknown error occurred.");
-            break;
-    }
+		        case error.PERMISSION_DENIED:
+		            alert("User denied the request for Geolocation.");
+		            break;
+		        case error.POSITION_UNAVAILABLE:
+		            alert("Location information is unavailable.");
+		            break;
+		        case error.TIMEOUT:
+		            alert("The request to get user location timed out.");
+		            break;
+		        case error.UNKNOWN_ERROR:
+		            alert("An unknown error occurred.");
+		            break;
+	    }
 }
 
-//getLocation();
+function fakePost() {
+	var url = 'http://jsonplaceholder.typicode.com/posts';
+
+	var xhr;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xhr.open("GET", url, true);
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			console.log(xhr);
+			console.log("Respuesta servidor", JSON.parse(xhr.responseText));
+		}
+	}
+
+	xhr.send();
+}
+
+// fakePost();
+
+getLocation();
