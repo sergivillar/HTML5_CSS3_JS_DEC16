85b021418ba4ae86422f775c1ad9e284d53de92e
Sergio Fernandez <sergio@kasfactory.net>
2016-12-13 21:02:35 +0100

    Added camera API HTML 5

---
+++ js/camera.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", function(event) {
+	var video = document.getElementById("video");
+	var videoObj = { "video": true };
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+
+	var errBack = function (error) {
+		console.log("Video capture error: ", error.code); 
+	};
+
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia(videoObj, function(stream) {
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+
+	document.getElementById("snap").addEventListener("click", function(event) {
+		context.drawImage(video, 0, 0, 600, 400);
+	});
+});
